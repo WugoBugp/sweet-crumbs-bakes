047509481399982c64bdc46966cff9ff2e56dc44
WugoBugp <wyattbaker1512@gmail.com>
2026-05-17 17:37:25 -0500

    Update Stripe redirect URLs

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ app.post("/create-checkout-session", async (req, res) => {
         },
         quantity: item.quantity,
       })),
-      success_url: "http://localhost:5173/success",
-      cancel_url: "http://localhost:5173",
+      success_url: "https://cookieforge.org/success",
+      cancel_url: "https://cookieforge.org",
     })
 
     res.json({ url: session.url })
