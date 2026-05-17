1fe735a5c6085c7425614689966cfae0ea0c1966
WugoBugp <wyattbaker1512@gmail.com>
2026-05-16 21:57:12 -0500

    Fix component import capitalization

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Hero from "./components/Hero"
-import FeaturedProducts from "./components/FeaturedProducts"
-import MenuCategories from "./components/MenuCategories"
-import SixPackBanner from "./components/SixPackBanner"
-import CookieBuilder from "./components/CookieBuilder"
-import Footer from "./components/Footer"
+import Hero from "./Components/Hero"
+import FeaturedProducts from "./Components/FeaturedProducts"
+import MenuCategories from "./Components/MenuCategories"
+import SixPackBanner from "./Components/SixPackBanner"
+import CookieBuilder from "./Components/CookieBuilder"
+import Footer from "./Components/Footer"
 
 export default function App() {
   return (
